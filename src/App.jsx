9481e341159f1024aa3ad6b66cf78e2e4bc06ebf
Diego Vistoso <diegoivg98@gmail.com>
2022-12-18 19:17:32 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+
+function App() {
+  const [binary, setBinary] = useState("");
+  const [decimal, setDecimal] = useState("");
+
+  function handleChange(event) {
+    setBinary(event.target.value);
+  }
+
+  function handleConversion() {
+    /* Convertir el número binario en decimal. */
+    setDecimal(parseInt(binary, 2));
+  }
+
+  return (
+    <div className="container mt-3">
+      <h1 className="text-center p-2">Bin2Dec</h1>
+      <div className="form-group">
+        <input
+          type="number"
+          className="form-control"
+          id="binary-input"
+          value={binary}
+          onChange={handleChange}
+          placeholder="Ingresa el número binario"
+          required
+        />
+      </div>
+      <button
+        type="button"
+        className="btn btn-outline-success d-flex m-auto mt-sm-3"
+        onClick={handleConversion}
+      >
+        Convertir
+      </button>
+      <div className="form-group mt-3">
+        <input
+          type="text"
+          className="form-control"
+          id="decimal-output"
+          value={decimal}
+          readOnly
+          placeholder="Resultado en decimal"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
